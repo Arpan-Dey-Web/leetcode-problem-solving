fbea85d4e2a6cf97ad491f695a91047a8d417c36
Arpan-Dey-Web <arpandey.web@gmail.com>
2025-09-11 01:18:34 +0600

    basic leetcode problem solving start

---
+++ leet01.js
@@ -0,0 +1,10 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+
+let createHelloWorld = function () {
+    return function () {
+        return "Hello World";
+    }
+}
+
+console.log(createHelloWorld()());
